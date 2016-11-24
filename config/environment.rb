e3f55630aeedb1e08bc355b7fef8f0d24579ede8
Rails Udemy Course <zai4sems@gmail.com>
2016-11-24 17:31:10 +0000

    fix email error

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrip.net',
-    :port => '587',
-    :authentication => :plain,
-    :username => ENV['SENDGRIP_USERNAME'],
-    :password => ENV['SENDGRIP_PASSWORD'],
+   
+    
+    
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
     :domain => 'heroku.com',
-    :enable_starttls_auto => true
 }
